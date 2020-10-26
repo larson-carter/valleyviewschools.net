21d7964d3bc8c9de1aae03831377da9a6dbd6e86
Larson Carter <larsoncarter@blazermail.net>
2020-10-25 21:43:26 -0500

    Setup Files and Routes
    I setup the basic outline for all of the files. I also setup all of the parent routes.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
+    alias: '/home',
     name: 'Home',
     component: Home
   },
@@ -14,11 +15,51 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/administration',
+    name: 'Administration',
+    component: () => import(/* webpackChunkName: "Administration" */ '../views/Administration/Administration.vue')
+  },
+  {
+    path: '/calendar',
+    name: 'Calendar',
+    component: () => import(/* webpackChunkName: "Calendar" */ '../views/Calendar/Calendar.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import(/* webpackChunkName: "Contact" */ '../views/Contact/Contact.vue')
+  },
+  {
+    path: '/departments',
+    name: 'Departments',
+    component: () => import(/* webpackChunkName: "Departments" */ '../views/Departments/Departments.vue')
+  },
+  {
+    path: '/resources',
+    name: 'Resources',
+    component: () => import(/* webpackChunkName: "Resources" */ '../views/Resources/Resources.vue')
+  },
+  {
+    path: '/schools',
+    name: 'Schools',
+    component: () => import(/* webpackChunkName: "Schools" */ '../views/Schools/Schools.vue')
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: () => import(/* webpackChunkName: "Search" */ '../views/Search/Search.vue')
+  },
+  {
+    path: '/state-required-information',
+    name: 'State Required Information',
+    component: () => import(/* webpackChunkName: "State Required Information" */ '../views/State-Required-Information/State-Required-Information.vue')
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
